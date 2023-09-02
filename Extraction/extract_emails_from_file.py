782b28e38adcd47eb8929ab8b29688b26a6937f2
Felix.T <75949095+Qyfashae@users.noreply.github.com>
2023-09-02 15:02:19 +0100

    Update extract_emails_from_file.py

--- Extraction/extract_emails_from_file.py
+++ Extraction/extract_emails_from_file.py
@@ -1,33 +1,43 @@
 import re
-fileToRead = 'filetoread.txt'
-fileToWrite = 'filetoextractemailstoo.txt'
-delimiterInFile = [',', ';']
+
+fileToRead = "filetoread.txt"  # File to extract from
+fileToWrite = "filetowrite.txt"  # File to write to
+delimiterInFile = [
+    ",",
+    ";",
+]  # Change here whether it is for example email@email.com;nextemail@nextemail.com or ';'
+
+
 def validateEmail(strEmail):
-    # .* Zero or more characters of any type. 
-    if re.match("[a-zA-Z0-9.!#$%&'+-/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)", strEmail):
+    if re.match(
+        "[a-zA-Z0-9.!#$%&'+-/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)", strEmail
+    ):
         return True
     return False
+
+
 def writeFile(listData):
-    file = open(fileToWrite, 'w+')
+    file = open(fileToWrite, "w+")
     strData = ""
     for item in listData:
-        strData = strData+item+'\n'
+        strData = strData + item + "\n"
     file.write(strData)
+
+
 listEmail = []
-file = open(fileToRead, 'r') 
+file = open(fileToRead, "r")
 listLine = file.readlines()
 for itemLine in listLine:
-    item =str(itemLine)
+    item = str(itemLine)
     for delimeter in delimiterInFile:
-        item = item.replace(str(delimeter),' ')
-
+        item = item.replace(str(delimeter), " ")
     wordList = item.split()
     for word in wordList:
-        if(validateEmail(word)):
+        if validateEmail(word):
             listEmail.append(word)
 if listEmail:
     uniqEmail = set(listEmail)
-    print(len(uniqEmail),"emails collected!")
+    print(len(uniqEmail), "emails collected!")
     writeFile(uniqEmail)
 else:
     print("No email found.")
